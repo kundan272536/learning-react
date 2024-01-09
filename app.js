288968ece1109a88ce5580ab89d0bac8a265492f
KUNDAN KUMAR <103889218+kundan272536@users.noreply.github.com>
2024-01-09 19:50:21 +0530

    completed jsx, react component, babel package

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-    "div", { id: "parent" },
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", { id: "heading" }, "Hello from the React world123🧨❤️"),React.createElement("h1", { id: "heading" }, "Hello from the React world")]
-        ));
-console.log(heading, "heading is here");
+// const heading = React.createElement
+//     ("div", { id: "parent" },
+//         React.createElement("div", { id: "child" },
+//             [React.createElement("h1", { id: "heading" }, "Hello from the React world123🧨❤️"),
+//             React.createElement("h1", { id: "heading" }, "Hello from the React world")]
+//         ));
+// console.log(heading, "heading is here");
+
+//  *React Element *
+// const jsxHeading=(
+// <h1 id="heading">Hello from the JSXHeading</h1>
+// );
+const elem = <span>React Element</span>;
+const title = ({ elem }, <h1>This is Title from React Element</h1>);
+const Title = () => <h2>This is the Title from React Component</h2>;
+//React component
+const Heading = () =>
+  <div className="container">
+    <Title />
+    {/* <Title></Title> */}
+    {/* {Title()} */}
+    {title}
+    <h1>Hello this is from function component</h1>
+    <h2>fkdsfjds</h2>
+  </div>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root)
-root.render(heading);
+console.log(root);
+// root.render(jsxHeading);//Rendering the react element
+root.render(<Heading />);
